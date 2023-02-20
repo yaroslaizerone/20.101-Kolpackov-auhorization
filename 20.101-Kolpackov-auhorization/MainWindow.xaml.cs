7e8d2005a8db77823f2a6307d640ed285b413b66
pr-20.101-kia <pr-20.101-kia@348-10>
2023-02-20 18:00:02 +0700

    Добавьте файлы проекта.

---
+++ 20.101-Kolpackov-auhorization/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20._101_Kolpackov_auhorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string UserLogin = TBLogin.Text.ToString();
+            string UserPassword = TBPassword.Text.ToString();
+            Users users = new Users();
+
+            try
+            {
+                users = DataEntities.GetContext().Users.Where(x => x.Login == UserLogin && x.Password == UserPassword).FirstOrDefault();
+                if (users != null)
+                {
+                    if (users.GetNamePost == "Администратор")
+                    {
+                        OutputResult("Администратор");
+                    }
+                    else if (users.GetNamePost == "Менеджер")
+                    {
+                        OutputResult("Менеджер");
+                    }
+                    else if (users.GetNamePost == "Клиент")
+                    {
+                        OutputResult("Клиент");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль!");
+                
+                }   
+            }
+            catch
+            {
+                MessageBox.Show("Неверный логин или пароль!");
+            }
+            
+        }
+
+        public string OutputResult(string str)
+        {
+            MessageBox.Show($"Добро пожаловать! Ваша роль: {str}");
+            return str;
+        }
+    }
+}
